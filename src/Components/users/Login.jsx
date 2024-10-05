3fbd98982ab8876db6a5f1f6bbf9528464f03834
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-10-05 12:21:37 +0530

    first commit

---
+++ src/Components/users/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className='text-6xl bg-black text-white h-screen flex justify-center items-center'>
+            Login Page
+        </div>
+    )
+}
+
+export default Login
+
